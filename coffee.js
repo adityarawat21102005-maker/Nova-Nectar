41ccada1903a0600a34cc195823e3f49fe556069
Aditya Rawat <adityarawat21102005@gmail.com>
2026-02-17 14:46:09 +0530

    Initial commit

---
+++ coffee.js
@@ -0,0 +1,63 @@
+
+// console.log(displayClock);
+
+let displayClock = document.querySelector(".digitalClock span");
+
+let dClock = () => {
+    let date = new Date();
+
+    let hour = date.getHours();
+    let hourType = hour >= 12 ? "PM" : "AM";
+
+    hour = hour % 12;
+    hour = hour ? hour : 12; // 0 ko 12 bana do
+
+    let currentHour = hour < 10 ? "0" + hour : hour;
+    let currentMin = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    let currentSec = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+
+    displayClock.innerHTML = `${currentHour} : ${currentMin} : ${currentSec} ${hourType}`;
+};
+
+setInterval(dClock, 1000);
+
+// For Mobile Menu
+let menuBtn = document.getElementById("menuBtn");
+let navLinks = document.getElementById("navLinks");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+
+// For FAQ Section
+let questions = document.querySelectorAll(".faqInner h2");
+
+questions.forEach(function(question){
+
+    question.addEventListener("click", function(){
+
+        let paragraph = this.nextElementSibling;
+        let icon = this.querySelector("span");
+
+        // Check karo ye already open hai ya nahi
+        let isOpen = paragraph.classList.contains("faq-Show");
+
+        // Sab close karo
+        document.querySelectorAll(".faqInner p").forEach(function(p){
+            p.classList.remove("faq-Show");
+        });
+
+        document.querySelectorAll(".faqInner h2 span").forEach(function(s){
+            s.classList.remove("span-show");
+        });
+
+        // Agar pehle open nahi tha to open karo
+        if(!isOpen){
+            paragraph.classList.add("faq-Show");
+            icon.classList.add("span-show");
+        }
+
+    });
+
+});
